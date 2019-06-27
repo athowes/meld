264590f21c35e6f3068fd1931e398bd8b1972d40
Howes <u1864575@live.warwick.ac.uk>
2019-06-27 17:43:41 +0100

    Mainly optimal scaling attempts, adding acceptance rate, cleaning functions

--- logistic_regression.R
+++ logistic_regression.R
@@ -26,7 +26,7 @@ summary(mle)
 
 b <- mle$coefficients
 
-# Using rjags -------------------------------------------------------------
+# Posterior sampling using rjags ------------------------------------------
 
 # Specify the JAGS model
 
@@ -57,7 +57,8 @@ update(model, 10000)
 
 samp <- coda.samples(model, variable.names = c("beta"), 10000)
 
-# From scratch ------------------------------------------------------------
+
+# Metropolis-within-Gibbs -------------------------------------------------
 
 X <- cbind(intercept = 1, X) # Add intercept column to design matrix
 
@@ -94,51 +95,86 @@ mwg <- function(b0, X, mu, sigma, scale, nsim) {
   p <- length(b0)
   r <- array(NA, c(nsim, p))
   r[1, ] <- b0 # init
+  s <- array(0, c(3, p)) # Acceptance counts
   for(i in 2:nsim) {
     j <- sample(1:p, 1)
+    s[1, j] <- s[1, j] + 1 # Update pick count
     r[i, ] <- mh(r[i-1, ], X, mu, sigma, j, scale)
+    if(!identical(r[i, ], r[i-1, ])) s[2, j] <- s[2, j] + 1 # Update accept count
   }
   r <- as.data.frame(r)
   names(r) <- sprintf("b%d", 1:p)
-  return(r)
+  s[3, ] <- s[2, ] / s[1, ] # Acceptance rates
+  return(list("chain" = r, "accept" = s))
 }
 
-trace_plots_simple <- function(x, nsim) { # Helper function
-  a <- qplot(x = 1:nsim, y = x$b1, geom = "line")
-  b <- qplot(x = 1:nsim, y = x$b2, geom = "line")
-  c <- qplot(x = 1:nsim, y = x$b3, geom = "line")
-  d <- qplot(x = 1:nsim, y = x$b4, geom = "line")
-  e <- qplot(x = 1:nsim, y = x$b5, geom = "line")
-  f <- qplot(x = 1:nsim, y = x$b6, geom = "line")
-  cowplot::plot_grid(a, b, c, d, e, f, ncol = 3)
+# Function to calculate the total acceptance rate retroactively (this may not be so useful)
+accept_rate <- function(b, chains) {
+  lag <- rbind(as.matrix(t(b)), as.matrix(chains[-nrow(chains),]))
+  sum(rowSums(chains - lag) == 0) / nrow(chains)
 }
 
-# trace_plots <- function(x, p, nsim) { # Helper function
-#   for (i in 1:p) {
-#     assign(paste0("plot", i), qplot(x = 1:nsim, y = x[[paste0("b", i)]], geom = "line"))
-#   }
-#   # This isn't working yet...
-# }
+# Scaling considerations --------------------------------------------------
 
-test <- mwg(b, X, mu, sigma, scale = rep(0.1, 6), nsim = 10^4) # But this is starting at roughly the right values...
-trace_plots_simple(test, 10^4) # Seems like b2 and b5 are more sticky, maybe reduce scale here? 
+trace_plots <- function(x, nsim) { # Helper function
+  plot1 <- qplot(x = 1:nsim, y = x$chain$b1, geom = "line")
+  plot2 <- qplot(x = 1:nsim, y = x$chain$b2, geom = "line")
+  plot3 <- qplot(x = 1:nsim, y = x$chain$b3, geom = "line")
+  plot4 <- qplot(x = 1:nsim, y = x$chain$b4, geom = "line")
+  plot5 <- qplot(x = 1:nsim, y = x$chain$b5, geom = "line")
+  plot6 <- qplot(x = 1:nsim, y = x$chain$b6, geom = "line")
+  cowplot::plot_grid(plot1, plot2, plot3, plot4, plot5, plot6, ncol = 3)
+}
 
-scale2 <- c(0.1, 0.0005, 0.1, 0.1, 0.005, 0.1) # Tried a few configurations here to see what looks best
-test2 <- mwg(b, X, mu, sigma, scale = scale2, nsim = 10^4)
-trace_plots_simple(test2, 10^4)
+test <- mwg(b, X, mu, sigma, scale = rep(0.1, 6), nsim = 10^4) # Initialising at roughly the right values
+trace_plots(test, 10^4)
+test$accept # Plan is to adjust scale based on optimal acceptance rate 0.234
 
-# Okay lets give a larger sample size and starting at zero a go
+# Optimal scalings for each parameter should roughly be some constant multiplied by their standard error
+se <- sqrt(diag((vcov(mle))))
 
-chains <- mwg(rep(0, 6), X, mu, sigma, scale = scale2, nsim = 10^5)
-trace_plots_simple(chains, 10^5) # 1, 2, 5 could still be improved - not sure how - ask Murray, Pier?
-saveRDS(chains, "results/chains.Rds")
+# Objective function to be optimised
+g <- function(s, j) {
+  v <- rep(0.1, 6)
+  v[j] <- s
+  accept <- mwg(b, X, mu, sigma, scale = v, nsim = 10^3)$accept
+  (accept[3, j] - 0.234)^2
+}
 
-colMeans(chains); b # Pretty close to the ML estimates, so the code is probably correct mostly
+find_scale <- function(j) {
+  # Optimise the (stochastic) objective function 5 times then average
+  runs <- replicate(5, optimise(f = g, c(0, 0.1), j = j)$minimum)
+  k <- mean(runs) / se[j]
+  k * se # Guess at optimal scaling
+}
+
+opt_scale1 <- find_scale(1) # And so on..
+opt_scale2 <- find_scale(2) 
+opt_scale3 <- find_scale(3)
+
+# This seems not to be working well
+
+opt_scale1
+opt_scale2
+opt_scale3
+
+my_guess <- c(0.1, 0.0001, 0.0005, 0.05, 0.005, 0.01)
+
+test2 <- mwg(b, X, mu, sigma, scale = my_guess, nsim = 10^4)
+trace_plots(test2, 10^4)
+test2$accept
+
+# Larger sample size and starting at zero
+full <- mwg(rep(0, 6), X, mu, sigma, scale = my_guess, nsim = 10^5)
+trace_plots(full, 10^5) # 1, 2, 5 could still be improved - not sure how - ask Murray, Pier?
+saveRDS(full, "results/full_model.Rds")
+
+colMeans(full$chain); b # Pretty close to the ML estimates, so the code is probably correct mostly
 
 # Subset modelling --------------------------------------------------------
 
 head(X) # Reminder about the variables we have
-# Now try only using a subset of the predictors in each model
+# Only using a subset of the predictors in each model
 
 # |   | b1 | b2 | b3 | b4 | b5 | b6 |
 # |---|----|----|----|----|----|----|
@@ -173,32 +209,16 @@ mu2 <- rep(0, p2)
 sigma2 <- rep(0.1, p2)
 
 # Run the samplers
-chains1 <- mwg(b0 = rep(0, 4), X = X1, mu = mu1, sigma = sigma1,
-               scale = scale2[c(1, 2, 5, 6)], nsim = 10^5)
-names(chains1) <- c("b1", "b2", "b5", "b6") # Correct the naming
-saveRDS(chains1, "results/chains1.Rds")
+model1 <- mwg(b0 = rep(0, 4), X = X1, mu = mu1, sigma = sigma1,
+               scale = my_guess[c(1, 2, 5, 6)], nsim = 10^5)
+names(model1) <- c("b1", "b2", "b5", "b6") # Correct the naming
+saveRDS(model1, "results/model1.Rds")
 
-chains2 <- mwg(b0 = rep(0, 4), X = X2, mu = mu2, sigma = sigma2,
-               scale = scale2[c(1, 3, 4, 6)], nsim = 10^5)
-names(chains2) <- c("b1", "b3", "b4", "b6") # Correct the naming
-saveRDS(chains2, "results/chains2.Rds")
+model2 <- mwg(b0 = rep(0, 4), X = X2, mu = mu2, sigma = sigma2,
+               scale = my_guess[c(1, 3, 4, 6)], nsim = 10^5)
+names(model2) <- c("b1", "b3", "b4", "b6") # Correct the naming
+saveRDS(model2, "results/model2.Rds")
 
-# Trace-plots
-assign(paste0("m1_plot", 1), qplot(x = 1:100000, y = chains1[[paste0("b", 1)]], geom = "line"))
-assign(paste0("m1_plot", 2), qplot(x = 1:100000, y = chains1[[paste0("b", 2)]], geom = "line"))
-assign(paste0("m1_plot", 5), qplot(x = 1:100000, y = chains1[[paste0("b", 5)]], geom = "line"))
-assign(paste0("m1_plot", 6), qplot(x = 1:100000, y = chains1[[paste0("b", 6)]], geom = "line"))
+colMeans(model1$chains); b1
 
-cowplot::plot_grid(m1_plot1, m1_plot2, m1_plot5, m1_plot6)
-
-assign(paste0("m2_plot", 1), qplot(x = 1:100000, y = chains2[[paste0("b", 1)]], geom = "line"))
-assign(paste0("m2_plot", 3), qplot(x = 1:100000, y = chains2[[paste0("b", 3)]], geom = "line"))
-assign(paste0("m2_plot", 4), qplot(x = 1:100000, y = chains2[[paste0("b", 4)]], geom = "line"))
-assign(paste0("m2_plot", 6), qplot(x = 1:100000, y = chains2[[paste0("b", 6)]], geom = "line"))
-
-cowplot::plot_grid(m2_plot1, m2_plot3, m2_plot4, m2_plot6)
-
-colMeans(chains1); b1
-
-colMeans(chains2); b2 # Still getting close: good!
-
+colMeans(model2$chains); b2 # Still getting close: good!
